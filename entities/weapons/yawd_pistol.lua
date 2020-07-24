3bd1b9eabfa67c19d712b737084ea206c4294a7b
lewis <me@lewis.to>
2020-07-24 23:12:09 +0100

    Start on pistol

---
+++ entities/weapons/yawd_pistol.lua
@@ -0,0 +1,99 @@
+SWEP.Base = "weapon_base"
+SWEP.PrintName = "Pistol"
+SWEP.Author = "YAWD Team"
+SWEP.Category = "YAWD"
+SWEP.Spawnable = true
+SWEP.AdminOnly = true
+
+SWEP.m_WeaponDeploySpeed = 1
+
+SWEP.Slot = 0
+SWEP.SlotPos = 0
+SWEP.DrawCrosshair = true
+SWEP.DrawAmmo = true
+SWEP.Weight = 5
+
+SWEP.ViewModelFOV = 62
+SWEP.ViewModelFlip = false
+
+SWEP.UseHands = true
+SWEP.ViewModel = ""
+SWEP.WorldModel = ""
+SWEP.HoldType = "pistol"
+
+SWEP.PrimaryDelay = 0.4
+SWEP.PrimaryDamage = 10
+SWEP.PrimaryForce = 1
+SWEP.PrimaryMaxDistance = 56756
+SWEP.PrimarySound = "weapons/pistol/pistol_fire3.wav"
+SWEP.PrimaryViewPunch = {
+	p = {
+		min = -4,
+		max = -7,
+	},
+	y = {
+		min = -1,
+		max = 1,
+	},
+}
+
+SWEP.SecondaryDelay = 1
+
+function SWEP:Initialize()
+	self:SetHoldType(self.HoldType)
+end
+
+function SWEP:Deploy()
+
+end
+
+function SWEP:Holster()
+
+end
+
+function SWEP:PrimaryAttack()
+	if not self:CanPrimaryAttack() then
+		return
+	end
+
+	local owner = self:GetOwner()
+	local bullet_info = {
+		Attacker = owner,
+		Damage = self.PrimaryDamage,
+		Force = self.PrimaryForce,
+		Distance = self.PrimaryMaxDistance,
+		Num = 1,
+		Dir = owner:GetAimVector(),
+		Spread = Vector(0, 0, 0),
+		Src = owner:GetShootPos(),
+	}
+
+	self:FireBullets(bullet_info)
+	self:EmitSound(self.PrimarySound, 75, 100, 1, CHAN_WEAPON)
+	self:ViewPunch()
+
+	self:SetNextPrimaryFire(CurTime() + self.PrimaryDelay)
+end
+
+function SWEP:SecondaryAttack()
+	if not self:CanSecondaryAttack() then
+		return
+	end
+
+	self:SetNextSecondaryFire(CurTime() + self.SecondaryDelay)
+end
+
+function SWEP:Reload()
+
+end
+
+function SWEP:ViewPunch()
+	local owner = self:GetOwner()
+	local viewpunch = self.PrimaryViewPunch
+
+	owner:ViewPunch(Angle(
+		math.random(viewpunch.p.min, viewpunch.p.max),
+		math.random(viewpunch.y.min, viewpunch.y.max),
+		0
+	))
+end
