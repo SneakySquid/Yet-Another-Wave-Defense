2ea5a549c60736f279a00a0b41fd3df6340a016b
lewis <me@lewis.to>
2020-07-25 01:35:59 +0100

    Do ammo and reloading stuff for pistol

--- entities/weapons/yawd_pistol.lua
+++ entities/weapons/yawd_pistol.lua
@@ -17,12 +17,23 @@ SWEP.ViewModelFOV = 62
 SWEP.ViewModelFlip = false
 
 SWEP.UseHands = true
-SWEP.ViewModel = ""
-SWEP.WorldModel = ""
+SWEP.ViewModel = "models/weapons/v_pist_fiveseven.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_fiveseven.mdl"
 SWEP.HoldType = "pistol"
+SWEP.ViewModelFlip = true
 
-SWEP.PrimaryDelay = 0.4
+SWEP.Primary = {
+	Ammo = "Pistol",
+	ClipSize = 12,
+	DefaultClip = 48,
+	Automatic = true,
+}
+
+SWEP.Secondary = {}
+
+SWEP.PrimaryDelay = 0.2
 SWEP.PrimarySpread = Vector(0.03, 0.01, 0)
+SWEP.PrimaryBulletsPerFire = 1
 SWEP.PrimaryDamage = {
 	min = 7,
 	max = 14,
@@ -32,8 +43,8 @@ SWEP.PrimaryMaxDistance = 56756
 SWEP.PrimarySound = "weapons/pistol/pistol_fire3.wav"
 SWEP.PrimaryViewPunch = {
 	p = {
-		min = -4,
-		max = -7,
+		min = -1,
+		max = -3,
 	},
 	y = {
 		min = -1,
@@ -43,16 +54,18 @@ SWEP.PrimaryViewPunch = {
 
 SWEP.SecondaryDelay = 1
 
+SWEP.ReloadDelay = 1
+
 function SWEP:Initialize()
 	self:SetHoldType(self.HoldType)
 end
 
 function SWEP:Deploy()
-
+	return true
 end
 
 function SWEP:Holster()
-
+	return true
 end
 
 function SWEP:PrimaryAttack()
@@ -66,15 +79,20 @@ function SWEP:PrimaryAttack()
 		Damage = math.random(self.PrimaryDamage.min, self.PrimaryDamage.max),
 		Force = self.PrimaryForce,
 		Distance = self.PrimaryMaxDistance,
-		Num = 1,
+		Num = self.PrimaryBulletsPerFire,
 		Dir = owner:GetAimVector(),
 		Spread = self.PrimarySpread,
 		Src = owner:GetShootPos(),
 	}
 
+	self:TakePrimaryAmmo(self.PrimaryBulletsPerFire)
 	self:FireBullets(bullet_info)
 	self:EmitSound(self.PrimarySound, 75, 100, 1, CHAN_WEAPON)
 	self:ViewPunch()
+
+	self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+	owner:MuzzleFlash()
+	owner:SetAnimation(PLAYER_ATTACK1)
 
 	self:SetNextPrimaryFire(CurTime() + self.PrimaryDelay)
 end
@@ -88,7 +106,32 @@ function SWEP:SecondaryAttack()
 end
 
 function SWEP:Reload()
+	if not self:CanReload() then
+		return
+	end
 
+
+	self:DefaultReload(ACT_VM_RELOAD)
+
+	local owner = self:GetOwner()
+	owner:SetAnimation(PLAYER_RELOAD)
+
+	local time = CurTime()
+	self:SetNextReload(time + self.ReloadDelay)
+	self:SetNextPrimaryFire(time + self.PrimaryDelay)
+	self:SetNextSecondaryFire(time + self.SecondaryDelay)
+end
+
+function SWEP:CanReload()
+	return CurTime() >= self:GetNextReload()
+end
+
+function SWEP:SetNextReload(time)
+	self.next_reload = time
+end
+
+function SWEP:GetNextReload()
+	return self.next_reload or 0
 end
 
 function SWEP:ViewPunch()
