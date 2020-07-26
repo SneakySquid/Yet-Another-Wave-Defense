e3ee3ecd4ef05c8a73ca5e427a5efb2b21347592
lewis <me@lewis.to>
2020-07-26 15:52:32 +0100

    Add sniper swep

---
+++ entities/weapons/yawd_sniper.lua
@@ -0,0 +1,63 @@
+SWEP.Base = "yawd_sniper_base"
+SWEP.PrintName = "Sniper"
+SWEP.Author = "YAWD Team"
+SWEP.Category = "YAWD"
+SWEP.Spawnable = true
+SWEP.AdminOnly = true
+
+SWEP.m_WeaponDeploySpeed = 1
+
+SWEP.Slot = 0
+SWEP.SlotPos = 0
+SWEP.DrawCrosshair = true
+SWEP.DrawAmmo = true
+SWEP.Weight = 5
+
+SWEP.ViewModelFOV = 62
+SWEP.ViewModelFlip = false
+
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/v_snip_awp.mdl"
+SWEP.WorldModel = "models/weapons/w_snip_awp.mdl"
+SWEP.HoldType = "sniper"
+SWEP.ViewModelFlip = true
+
+SWEP.Primary = {
+	Ammo = "SniperRound",
+	ClipSize = 1,
+	DefaultClip = 16,
+	Automatic = true,
+}
+
+SWEP.ScopeOverlayMaterialPath = "overlays/scope_lens"
+SWEP.ScopeDelay = 0.1
+
+if CLIENT and #SWEP.ScopeOverlayMaterialPath > 0 then
+	SWEP.ScopeOverlayMaterial = Material(SWEP.ScopeOverlayMaterialPath)
+end
+
+SWEP.PrimaryDelay = 1
+SWEP.PrimaryUnscopedSpread = Vector(0.1, 0.1, 0)
+SWEP.PrimaryScopedSpread = Vector(0.005, 0.005, 0)
+SWEP.PrimaryBulletsPerFire = 1
+SWEP.PrimaryBulletsTakenPerShot = 1
+SWEP.PrimaryForce = 1
+SWEP.PrimaryMaxDistance = 56756
+SWEP.PrimarySound = "weapons/awp/awp1.wav"
+SWEP.PrimaryDamage = {
+	min = 90,
+	max = 110,
+}
+SWEP.PrimaryViewPunch = {
+	p = {
+		min = -0.5,
+		max = -1.4,
+	},
+	y = {
+		min = -0.4,
+		max = 0.4,
+	},
+}
+
+SWEP.ReloadDelay = 1
+SWEP.ReloadSound = "weapons/awp/awp_bolt.wav"
