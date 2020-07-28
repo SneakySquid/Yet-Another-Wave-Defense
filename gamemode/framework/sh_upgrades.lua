74a87380a69531547137f0e8ec6f1ab88ecc4a06
lewis <me@lewis.to>
2020-07-28 21:46:55 +0100

    Add upgrade helper functions

--- gamemode/framework/sh_upgrades.lua
+++ gamemode/framework/sh_upgrades.lua
@@ -20,13 +20,6 @@ local registered_upgrades = {}
 	price,			the price of the upgrade, can either be a number or a table for prices of each tier
 	can_purchase, 	optional function override for upgrades that aren't just restricted by price. args: tbd
 	on_purchase,	optional function that is called when a player purchases the upgrade
-	hooks,			optional table of hooks that allow this upgrade to function. format follows.
-					{
-						{
-							event		e.g. PlayerSpawn
-							callback
-						}
-					}
 }
 --]]
 function GM:RegisterUpgrade(t)
@@ -38,61 +31,104 @@ function GM:RegisterUpgrade(t)
 	_assert(isnumber(t.price) or istable(t.price), "price")
 	_assert(t.can_purchase == nil or isfunction(t.can_purchase), "can_purchase")
 	_assert(t.on_purchase == nil or isfunction(t.on_purchase), "on_purchase")
-	_assert(t.hooks == nil or istable(t.hooks), "hooks")
 
-	t.tiers = istable(t.price) and #t.price
-	t.hooks = t.hooks or {}
+	t.tiers = istable(t.price) and #t.price or nil
 
-	for _, v in ipairs(t.hooks) do
-		hook.Add(v.event, string.format("yawd.upgrade.%s", t.name), v.callback)
-	end
+	local k = table.insert(registered_upgrades, t)
+	t.k = k
 
-	table.insert(registered_upgrades, t)
+	DebugMessage(string.format("Registered upgrade '%s'", t.name))
 
-	DebugMessage(string.format("Registered upgrade '%s' with %d hooks", t.name, #t.hooks))
+	return k
 end
 
 function GM:GetUpgradesTable()
 	return registered_upgrades
 end
 
+function GM:GetUpgrade(key)
+	return registered_upgrades[key]
+end
+
+function GM:GetUpgradeByName(name)
+	for _, upgrade in ipairs(registered_upgrades) do
+		if upgrade.name == name then
+			return upgrade
+		end
+	end
+end
+
+-- NOTE: A client's upgrades aren't networked to other clients
+function GM:PlayerHasUpgrade(ply, key)
+	for _, upgrade in ipairs(ply.yawd_upgrades or {}) do
+		if upgrade.k == key then
+			return true, upgrade.tier or 1
+		end
+	end
+
+	return false
+end
+
+function GM:GetPlayerUpgrades(ply)
+	return ply.yawd_upgrades or {}
+end
+
+function GM:GetUpgradeRefundPercentage()
+	return 0.5
+end
+
+function GM:GetUpgradeRefundAmount(upgrade, purchased_upgrade_tiers)
+	local amt = 0
+
+	if isnumber(upgrade.price) then
+		amt = purchased_upgrade_tiers * upgrade.price
+	elseif istable(upgrade.price) then
+		for i = 1, math.min(purchased_upgrade_tiers, #upgrade.price) do
+			amt = amt + upgrade.price[i]
+		end
+	end
+
+	return amt * GAMEMODE:GetUpgradeRefundPercentage()
+end
+
 if SERVER then
 	AddCSLuaFile("upgrades/cl_upgrades.lua")
 
 	include("upgrades/sv_upgrades.lua")
+	include("upgrades/sv_net.lua")
 else
 	include("upgrades/cl_upgrades.lua")
 end
 
 ---------- Upgrades ----------
 
-GM:RegisterUpgrade({
+YAWD_UPGRADE_MOVEMENTSPEED = GM:RegisterUpgrade({
 	name = "Movement Speed",
 	price = {100, 200, 300, 400, 500},
 })
 
 -- TODO: There should probably be a resistance upgrade for each element
-GM:RegisterUpgrade({
+YAWD_UPGRADE_RESISTANCE = GM:RegisterUpgrade({
 	name = "Resistance",
 	price = 500,
 })
 
-GM:RegisterUpgrade({
+YAWD_UPGRADE_ARMOUR = GM:RegisterUpgrade({
 	name = "Armour",
 	price = 500,
 })
 
-GM:RegisterUpgrade({
+YAWD_UPGRADE_HEALTHREGEN = GM:RegisterUpgrade({
 	name = "Health Regen",
 	price = 500,
 })
 
-GM:RegisterUpgrade({
+YAWD_UPGRADE_MAXHEALTH = GM:RegisterUpgrade({
 	name = "Max Health",
 	price = 500,
 })
 
-GM:RegisterUpgrade({
+YAWD_UPGRADE_WEAPONCLIPSIZE = GM:RegisterUpgrade({
 	name = "Weapon Clip Size",
 	price = 500,
 })
