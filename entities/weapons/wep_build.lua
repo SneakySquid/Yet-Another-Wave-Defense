eb44eebb54722f6e03cb037b865039f9f1706ab3
Nak <nak@live.dk>
2020-07-29 09:24:53 +0200

    Weapon Selection system and wep_build
    - wep_build SWEP added for placment of traps.
    - WeaponSelection added.

---
+++ entities/weapons/wep_build.lua
@@ -0,0 +1,213 @@
+AddCSLuaFile()
+
+if CLIENT then
+   SWEP.PrintName          = "Building Wep"
+   SWEP.Slot               = 1
+
+   SWEP.ViewModelFlip      = false
+   SWEP.ViewModelFOV       = 54
+
+   SWEP.Icon               = "vgui/ttt/icon_glock"
+   SWEP.IconLetter         = "c"
+end
+
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo			= "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= true
+SWEP.Secondary.Ammo			= "none"
+
+SWEP.ShootSound = Sound( "NPC_CScanner.TakePhoto" )
+
+SWEP.UseHands              = true
+SWEP.ViewModel = 			"models/weapons/c_357.mdl"
+SWEP.WorldModel            = "models/weapons/w_pist_glock18.mdl"
+
+function SWEP:Initialize()
+	self:SetHoldType( "magic" )
+end
+
+if SERVER then
+	util.AddNetworkString("yawd_building_placment")
+	net.Receive("yawd_building_placment", function(len,ply)
+		-- Check for weapon
+		local wep = ply:GetActiveWeapon()
+		if not IsValid(wep) or wep:GetClass() ~= "wep_build" then return end
+		local building = net.ReadString()
+		local Rotate = net.ReadUInt(3)
+		-- Do we have the coins?
+		local bd = Building.GetData( building )
+		if not bd then return end -- Not a building
+		if not Building.CanPlayerBuild( ply, building ) then return end -- You can't build that
+		-- Get placment
+		local pos = ply:GetEyeTrace().HitPos
+		local succ, pos, ang = Building.CanPlaceOnFloor(building, pos, Rotate * 90, bAllowWater, net.ReadBool())
+		if not succ then return end -- You can't place it there
+		local b = Building.Create( building, ply , pos, ang )
+		if b then
+			b:EmitSound("ambient/machines/thumper_startup1.wav")
+		end
+		ply:SetCurrency(ply:GetCurrency() - (bd.Cost or 100))
+	end)
+else
+	function SWEP:SetBuilding(str)
+		self.Building = str
+		if IsValid(self.GhostEntity) then
+			self.GhostEntity:SetBuildingName( self:GetBuilding() )
+			self.GhostEntity.DrawSelection = nil
+			Building.ApplyFunctions( self.GhostEntity )
+		end
+	end
+	function SWEP:GetBuilding()
+		return self.Building
+	end
+	local base = Material("yawd/models/trap_base")
+	local side = Material("yawd/models/trap_side")
+	local TopMesh
+	local SideMesh
+	local Rotate = 0
+	local function GenerateMesh()
+		TopMesh = Mesh(Material("yawd/models/trap_base"))
+		local udata = {Vector(0,0,0),Vector(0,0,0),Vector(0,0,0),Vector(0,0,0)}
+		local function Quad(tab)	-- LT RT RB LB
+			local t = {}
+			table.insert(t, tab[1])
+			table.insert(t, tab[2])
+			table.insert(t, tab[3])
+			table.insert(t, tab[3])
+			table.insert(t, tab[4])
+			table.insert(t, tab[1])
+			return t
+		end
+		TopMesh:BuildFromTriangles(Quad({
+			{ pos = Vector( -0.5,-0.5,0.5 ), u = 0, v = 0, normal = vector_up , tangent = Vector(0, 1, 0), userdata = {-4.371,0.999,0,-1}},
+			{ pos = Vector( -0.5,0.5, 0.5 ), u = 0, v = 1, normal = vector_up , tangent = Vector(0, 1, 0), userdata = {-4.371,0.999,0,-1}},
+			{ pos = Vector( 0.5, 0.5, 0.5 ), u = 1, v = 1, normal = vector_up , tangent = Vector(0, 1, 0), userdata = {-4.371,0.999,0,-1}},
+			{ pos = Vector( 0.5,-0.5, 0.5 ), u = 1, v = 0, normal = vector_up , tangent = Vector(0, 1, 0), userdata = {-4.371,0.999,0,-1}}
+		}))
+		SideMesh = Mesh(Material("yawd/models/trap_side"))
+		local t = {}
+		local h = Vector(0,0,0.5)
+		local b = Vector(0,0,-10)
+		for i = 0, 3 do
+			local d = i * 90
+			local a = Angle(0,d,0)
+			local n = a:Forward()
+			local tan = -a:Right()
+			for _,t2 in ipairs(Quad({
+				{ pos = h + -n * 0.5 + tan *  0.5,     u = 0, v = 0, normal = -n,tangent = tan, userdata = {0.999, 4.37, 0,-1}}, -- LT
+				{ pos = h + -n * 0.5 + tan * -0.5,     u = 1, v = 0, normal = -n,tangent = tan, userdata = {0.999, 4.37, 0,-1}}, -- RT
+				{ pos =-h + -n * 0.5 + tan * -0.5 + b, u = 1, v = 1, normal = -n,tangent = tan, userdata = {0.999, 4.37, 0,-1}}, -- RB
+				{ pos =-h + -n * 0.5 + tan *  0.5 + b, u = 0, v = 1, normal = -n,tangent = tan, userdata = {0.999, 4.37, 0,-1}}  -- LB
+				})) do
+				table.insert(t, t2)
+			end
+		end
+		SideMesh:BuildFromTriangles(t)
+	end
+	function SWEP:RemoveGhost()
+		if self.GhostEntity then
+			SafeRemoveEntity(self.GhostEntity)
+		end
+		self.GhostEntity = nil
+	end
+	function SWEP:MakeGhost()
+		if not IsValid(self.GhostEntity) then
+			local ent = ents.CreateClientside( "yawd_building_ghost")
+			if self:GetBuilding() then
+				ent:SetBuildingName( self:GetBuilding() )
+				Building.ApplyFunctions(ent)
+			end
+			self.GhostEntity = ent
+		end
+	end
+	function SWEP:Deploy()		self:MakeGhost() return end
+	function SWEP:Holster()		self:RemoveGhost() return end
+	function SWEP:OnRemove()	self:RemoveGhost() return end
+	local m_Reload = false
+	function SWEP:Think()
+		self:MakeGhost()
+		local b = LocalPlayer():KeyDown( IN_RELOAD ) or LocalPlayer():KeyDown( IN_ATTACK2 )
+		if m_Reload~=b then
+			m_Reload = b
+			if m_Reload then
+				Rotate = (Rotate + 1) % 4
+				self:EmitSound("garrysmod/ui_click.wav")
+			end
+		end
+	end
+	local mat_invalid = Material("effects/select_dot")
+	function SWEP:DrawBuilding()
+		local bldData = Building.GetData( self:GetBuilding() )
+		if not bldData then return end -- Invaliod building
+		local ow = self:GetOwner()
+		local tr = ow:GetEyeTrace()
+		local pos = tr.HitPos
+		if tr.Entity and tr.Entity:GetClass() == "yawd_building" then
+			if IsValid(self.GhostEntity) then
+				self.GhostEntity:SetNoDraw(true)
+			end
+		else
+			local succ, pos, ang = Building.CanPlaceOnFloor(self:GetBuilding(), pos, Rotate * 90, bAllowWater, input.IsShiftDown())
+			if not succ then
+				if IsValid(self.GhostEntity) then
+					self.GhostEntity:SetNoDraw(true)
+				end
+				render.SetMaterial(mat_invalid)
+				render.DrawSprite(pos, 20,20, Color(255,0,0))
+			else
+				local ghost = self.GhostEntity
+				if IsValid(ghost) then
+					ghost:SetNoDraw(false)
+					ghost:SetPos(pos)
+					ghost:SetAngles(ang)
+				end
+			end
+		end
+	end
+	function SWEP:DrawWorldModel()
+	end
+	function SWEP:CalcViewModelView( vm, oldPos, oldAng, pos, ang )
+		return pos + Vector(0,0,0), ang
+	end
+	function SWEP:PreDrawViewModel()
+		render.SetBlend(0)
+	end
+	function SWEP:ViewModelDrawn()
+		render.SetBlend(1)
+	end
+	function SWEP:PostDrawViewModel()
+		render.SetBlend(1)
+		self:DrawBuilding()
+	end
+	local n_timer = 0
+	function SWEP:PrimaryAttack()
+		if n_timer > CurTime() then return end
+		n_timer = CurTime() + 1
+		-- Do we have the coins and class match?
+		if not Building.CanPlayerBuild( LocalPlayer(), self:GetBuilding() ) then 
+			self:EmitSound("buttons/button8.wav")
+			return 
+		end
+		-- Make sure we can place the building first
+		local pos = LocalPlayer():GetEyeTrace().HitPos
+		local succ, pos, ang = Building.CanPlaceOnFloorFast(self:GetBuilding(), pos, 0, bAllowWater, input.IsShiftDown())
+		if not succ then 
+			return 
+		end
+		-- Tell the server we want to place it
+		net.Start("yawd_building_placment")
+			net.WriteString(self:GetBuilding())
+			net.WriteUInt(Rotate, 3)
+			net.WriteBool(input.IsShiftDown())
+		net.SendToServer()
+	end
+	local n2_timer = 0
+	function SWEP:SecondaryAttack()
+	end
+	
+end
---
+++ gamemode/yawd/cl_weaponselect.lua
@@ -0,0 +1,201 @@
+
+do
+	surface.CreateFont("HUD.WeaponSelect", {
+		font = "Tahoma",
+		size = 18,
+		weight = 1500,
+		outline = true,
+		antialias = true,
+		additive = false,
+	})
+	surface.CreateFont("HUD.WeaponSelectCant", {
+		font = "Tahoma",
+		size = 18,
+		weight = 1500,
+		outline = true,
+		antialias = true,
+		additive = false,
+	})
+end
+
+hook.Add("HUDShouldDraw", "YAWD_WEPSelect", function( name )
+	if name == "CHudWeaponSelection" then return false end
+end)
+
+local function SwitchWeapon( wepName )
+	local active = LocalPlayer():GetActiveWeapon()
+	if active and IsValid(active) and active:GetClass() == wepName then return end
+	local wep = LocalPlayer():GetWeapon( wepName )
+	if not wep or not IsValid(wep) then return end
+	input.SelectWeapon( wep )
+end
+local buildings = {}
+local weps = {}
+local wep_slots = {}
+local function UpdateSlot()
+	wep_slots = {}
+	for k,v in ipairs(weps) do
+		if v[1] == "wep_build" then continue end
+		table.insert(wep_slots, v)
+	end
+	for k,v in ipairs(buildings) do
+		table.insert(wep_slots, v)
+	end
+end
+-- Updates the weapons and sorts them in a list. PlayerClassChanged doesn't have weapons
+local sc = {}
+local function UpdateWeapons()
+	weps = {}
+	sc = {}
+	for _,wep in ipairs(LocalPlayer():GetWeapons()) do
+		local p = wep:GetSlotPos() * 10 + wep:GetSlot()
+		if string.find(wep:GetClass(), "fist") then
+			p = -1
+		end
+		table.insert(weps, {wep:GetClass(),p, true})
+	end
+	table.sort(weps, function(a, b) return a[2]< b[2] end)
+	for id,k in ipairs(weps) do
+		local wep = weps[id][1]
+		table.insert(sc, wep )
+		weps[id][2] = (weapons.GetStored(wep) or {}).Cost or 100
+	end
+	UpdateSlot()
+end
+-- Yeee there are no hooks to call when the loadout changes
+timer.Create("yawd_loadoutscanner", 1, 0, function()
+	if not IsValid(LocalPlayer()) then return end
+	local wep_list = LocalPlayer():GetWeapons()
+	if #sc ~= #wep_list then
+		UpdateWeapons()
+	else
+		for _,wep in ipairs( wep_list ) do
+			if not table.HasValue(sc, wep:GetClass()) then
+				UpdateWeapons()
+				break
+			end
+		end
+	end
+end)
+local lastClass = -1
+local function UpdateBuildings()
+	if not LocalPlayer().GetPlayerClass then return end
+	local class = LocalPlayer():GetPlayerClass()
+	if lastClass == class then return end
+	lastClass = class
+	buildings = {}
+	for _,BuildingName in ipairs(Building.GetAll()) do
+		if not Building.CanClassBuild(BuildingName, class) then continue end
+		table.insert(buildings, {BuildingName, Building.GetData( BuildingName ).Cost})
+	end
+	table.sort(buildings,function(a,b) return a[2] < b[2] end)
+	UpdateSlot()
+end
+local selected = 1
+timer.Create("yawd_loadout", 0.2, 0, function()
+	-- Update buildings in case we switch class
+	UpdateBuildings()
+	if not wep_slots[selected] then selected = 1 return end -- In case something goes wrong
+	local wep = wep_slots[selected][1]
+	local wep_class
+	if wep_slots[selected][3] then
+		wep_class = wep
+	else
+		wep_class = "wep_build"
+		local wep_build = LocalPlayer():GetWeapon( "wep_build" )
+		if wep_build and IsValid(wep_build) then
+			wep_build:SetBuilding( wep )
+		end
+	end
+	if not wep_class then return end
+	SwitchWeapon( wep_class )
+end)
+local box_size = 80
+local box_space = 15
+local hover = Material("vgui/spawnmenu/hover")
+local hover_size = 8
+
+local t = {}
+local function SMaterial(str)
+	if t[str] then return t[str] end
+	t[str] = Material( str )
+	return t[str]
+end
+
+hook.Add("HUDPaint", "YAWD_WeaponSelection", function()
+	local ply = LocalPlayer()
+	if not IsValid(ply) then return end
+	if #ply:GetWeapons() <= 0 then return end
+	local wide = (#wep_slots + 1) * (box_size + box_space)
+	local wep_build = ply:GetWeapon( "wep_build" )
+	local Has_Wep_Build = wep_build and IsValid(wep_build)
+	surface.SetDrawColor(255,255,255)
+	local coin = ply:GetCurrency()
+	for k,v in ipairs(wep_slots) do
+		local x = k * (box_size + box_space) + ScrW() / 2 - wide / 2
+		local y = ScrH() * 0.8
+		local obj = v[1]
+		local CanAffort = coin >= v[2]
+		local IsWeapon =  v[3]
+		if k == selected then
+			surface.SetDrawColor(Color(255,255,255))
+			surface.SetMaterial(hover)
+			surface.DrawTexturedRect(x - hover_size, y - hover_size,box_size + hover_size * 2,box_size + hover_size * 2)
+		end
+		if IsWeapon then
+			surface.SetDrawColor(255, 255, 255, CanAffort and 255 or 155)
+			surface.SetMaterial(SMaterial("materials/entities/"..obj..".png"))
+			surface.DrawTexturedRect(x, y,box_size,box_size)
+			if v[2] > 0 then
+				if CanAffort then
+					surface.SetDrawColor(255, 255, 255, 255)
+					surface.DrawTexturedRect(x, y,box_size,box_size)
+					draw.DrawText(v[2], "HUD.WeaponSelect", x + box_size / 2, y + box_size - 18, Color( 255, 255, 255, 255 ) , TEXT_ALIGN_CENTER)
+				else
+					surface.SetDrawColor(155, 155, 155, 205)
+					surface.DrawTexturedRect(x, y,box_size,box_size)
+					draw.DrawText(v[2], "HUD.WeaponSelectCant", x + box_size / 2, y + box_size - 18, Color( 155, 155, 155, 255 ), TEXT_ALIGN_CENTER)
+				end
+			end
+		elseif Has_Wep_Build then
+			surface.SetMaterial(SMaterial("yawd/hud/"..obj..".png"))
+			if CanAffort then
+				surface.SetDrawColor(255, 255, 255, 255)
+				surface.DrawTexturedRect(x, y,box_size,box_size)
+				draw.DrawText(v[2], "HUD.WeaponSelect", x + box_size / 2, y + box_size - 18, Color( 255, 255, 255, 255 ) , TEXT_ALIGN_CENTER)
+			else
+				surface.SetDrawColor(155, 155, 155, 205)
+				surface.DrawTexturedRect(x, y,box_size,box_size)
+				draw.DrawText(v[2], "HUD.WeaponSelectCant", x + box_size / 2, y + box_size - 18, Color( 155, 155, 155, 255 ), TEXT_ALIGN_CENTER)
+			end
+		end
+		local hKey = k
+		if k == 10 then
+			k = 0
+		elseif k > 10 then
+			k = ""
+		end
+		draw.DrawText(k, "HUD.WeaponSelect", x, y, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT)
+	end
+end)
+
+local nextTurn = 0
+hook.Add("CreateMove", "YAWD_WeaponSelection", function(cmd)
+	for i = 0, 9 do
+		local key = KEY_0 + i
+		if input.WasKeyReleased( key ) then
+			selected = i > 0 and (key - 1) or 10
+			nextTurn = CurTime() + .05
+		end
+	end
+	local mvh = cmd:GetMouseWheel()
+	if mvh == 0 then return end
+	if nextTurn >= CurTime() then return end
+	nextTurn = CurTime() + .05
+	selected = selected - mvh
+	if selected <= 0 then
+		selected = #wep_slots
+	elseif selected > #wep_slots then
+		selected = 1
+	end
+end)
