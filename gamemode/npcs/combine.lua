5370c4cc0842eb4c316f21293d54f688d3e869cd
Nak <nak@live.dk>
2020-08-09 02:23:21 +0200

    Uses SpeakSnd

--- gamemode/npcs/combine.lua
+++ gamemode/npcs/combine.lua
@@ -20,7 +20,7 @@ function combine:Init()
 	self:GiveWeapon("weapon_smg1")
 end
 function combine:OnAttack( target )
-	self:EmitSound("npc/metropolice/vo/pickupthecan" .. math.random(1,3) .. ".wav")
+	self:SpeakSnd("npc/metropolice/vo/pickupthecan" .. math.random(1,3) .. ".wav")
 	self:PlaySequenceAndWait("combatidle1_smg1")
 	if not IsValid(target) then return end
 	for i = 1, 10 do
