c31c346f4342149bead6e020f658b1cc9e03c7ac
Nak <nak@live.dk>
2020-08-15 05:13:26 +0200

    Added cheat-commands.

---
+++ gamemode/yawd/sh_commands.lua
@@ -0,0 +1,25 @@
+-- Adds a few concommands
+-- Gives currency
+concommand.Add( "yawd_give_currency", function(ply, cmd, args)
+	local num = tonumber(args) or 0
+	if not ply then
+		for k,v in ipairs( player.GetAll() ) do
+			v:AddCurrency(num)
+		end
+	elseif IsValid( ply ) then
+		ply:AddCurrency(num)
+	end
+end, nil, "Gives currency", FCVAR_CHEAT )
+
+-- Sets the wavenumber
+concommand.Add( "yawd_set_wave", function(ply, cmd, args)
+	local num = tonumber(args) or 0
+	GAMEMODE:SetWaveNumber( num )
+end, nil, "Sets the wave number", FCVAR_CHEAT )
+
+-- Deletes all NPCs from current wave
+concommand.Add( "yawd_delete_npcs", function(ply, cmd, args)
+	for k,v in ipairs( ents.FindByClass("yawd_npc_base") ) do
+		SafeRemoveEntity( v )
+	end
+end, nil, "Deletes all NPCs", FCVAR_CHEAT )
