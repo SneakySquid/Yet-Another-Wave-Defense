58290ad18275c0b83ae7be08c97cf5cedcdc8617
Nak <nak@live.dk>
2020-08-15 06:12:17 +0200

    Fixed commands

--- gamemode/yawd/sh_commands.lua
+++ gamemode/yawd/sh_commands.lua
@@ -1,6 +1,6 @@
 -- Adds a few concommands
 -- Gives currency
-concommand.Add( "yawd_give_currency", function(ply, cmd, args)
+concommand.Add( "yawd_give_currency", function(ply, cmd,_ , args)
 	local num = tonumber(args) or 0
 	if not ply then
 		for k,v in ipairs( player.GetAll() ) do
@@ -12,13 +12,13 @@ concommand.Add( "yawd_give_currency", function(ply, cmd, args)
 end, nil, "Gives currency", FCVAR_CHEAT )
 
 -- Sets the wavenumber
-concommand.Add( "yawd_set_wave", function(ply, cmd, args)
+concommand.Add( "yawd_set_wave", function(ply, cmd,_ , args)
 	local num = tonumber(args) or 0
 	GAMEMODE:SetWaveNumber( num )
 end, nil, "Sets the wave number", FCVAR_CHEAT )
 
 -- Deletes all NPCs from current wave
-concommand.Add( "yawd_delete_npcs", function(ply, cmd, args)
+concommand.Add( "yawd_delete_npcs", function(ply, cmd)
 	for k,v in ipairs( ents.FindByClass("yawd_npc_base") ) do
 		SafeRemoveEntity( v )
 	end
